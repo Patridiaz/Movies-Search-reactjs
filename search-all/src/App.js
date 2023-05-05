9d59a78a0a6d8351c12d93c04d2cf37ea7c00842
Patridiaz <pdiaz290@gmail.com>
2023-05-05 00:58:10 -0400

    Api movie and show results

---
+++ search-all/src/App.js
@@ -0,0 +1,43 @@
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
+import { Component } from 'react';
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+
+class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
+  _renderResults() {
+    const { results } = this.state
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title> Searching...</Title>
+
+        <div className='SearchForm-wrapper'>
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin Resultados</p>
+          : this._renderResults()
+        }
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
---
+++ search-all/src/components/SearchForm.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+
+const API_KEY = 'af1dc589'
+
+export class SearchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange = (e) => {
+        this.setState({ inputMovie: e.target.value })
+    }
+
+    _handleSubmit= (e) => {
+        e.preventDefault()
+        const { inputMovie } = this.state   
+
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+        .then( res => res.json())
+        .then(results => {
+            const { Search, totalResults } = results
+            console.log({Search, totalResults })
+            this.props.onResults(Search)
+        })
+    }
+
+    render () {
+        return (
+            <form onSubmit={ this._handleSubmit}>
+            <div class="field has-addons">
+            <div class="control">
+              <input className="input"
+              onChange={this._handleChange} 
+              type="text" 
+              placeholder="Movie to search..!"/>
+            </div>
+            <div class="control">
+              <button class="button is-info">
+                Search
+              </button>
+            </div>
+          </div>
+          </form>
+        )
+    }
+}
